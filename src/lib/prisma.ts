fc7eed8b3532dde0a341a8c5ee54b91cef9cdcba
Shlok Choudhari <shlok999choudhari@gmail.com>
2026-08-14 12:25:09 +0530

    security issues fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,12 +4,6 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL || "postgresql://postgres.jfkfvpnsddcbyxvpbywn:Shlok788767@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
-      }
-    }
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
