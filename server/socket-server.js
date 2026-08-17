5870622938864a59c52d39d830810332683952ba
Shlok Choudhari <shlok999choudhari@gmail.com>
2026-08-18 01:13:31 +0530

    feat: implement Socket.IO signaling server and add prisma generate postinstall script

--- server/socket-server.js
+++ server/socket-server.js
@@ -86,7 +86,7 @@ io.on('connection', (socket) => {
   })
 })
 
-const PORT = 3001
+const PORT = process.env.SOCKET_PORT || 3001
 httpServer.listen(PORT, () => {
   console.log(`Socket.IO server running on port ${PORT}`)
 })
